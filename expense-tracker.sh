199e5cf9c970a7a425a327126e2460e7f9a4eeb1
Mulan007 <mbella@ext.uber.com>
2019-04-06 22:44:34 -0700

    add ExpenseItemTrigerHandler

--- expense-tracker.sh
+++ expense-tracker.sh
@@ -6,7 +6,7 @@ set -e
 
 # setup local development
 if [[ $1 = 'up' ]]; then
-    sfdx force:org:create -f config/project-scratch-def.json -a dev -s -d 21
+    sfdx force:org:create -f config/project-scratch-def.json -a newScratch -s -d 30
     sfdx force:data:tree:import --plan data/Expense_Category.json/export-demo-Category__c-plan.json
     sfdx force:source:push -f
     sfdx force:user:create -f config/consultant-user-def.json
